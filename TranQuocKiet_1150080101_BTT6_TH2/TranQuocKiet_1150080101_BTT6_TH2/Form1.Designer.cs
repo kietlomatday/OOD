6b8b1eda382fc352ac93c3195f75db04608120bd
Trần Quốc Kiệt <kiettran938@gmail.com>
2025-10-06 14:49:14 +0700

    Add files via upload

---
+++ TranQuocKiet_1150080101_BTT6_TH2/TranQuocKiet_1150080101_BTT6_TH2/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace TranQuocKiet_1150080101_BTT6_TH2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.groupBoxInput = new System.Windows.Forms.GroupBox();
+            this.lblDiaChi = new System.Windows.Forms.Label();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.lblLop = new System.Windows.Forms.Label();
+            this.txtLop = new System.Windows.Forms.TextBox();
+            this.dtpNgaySinh = new System.Windows.Forms.DateTimePicker();
+            this.lblNgaySinh = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.lblChucNang = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lvStudentList = new System.Windows.Forms.ListView();
+            this.lblThongTinChung = new System.Windows.Forms.Label();
+            this.groupBoxInput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(606, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "DANH MỤC SINH VIÊN";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // groupBoxInput
+            // 
+            this.groupBoxInput.Controls.Add(this.lblDiaChi);
+            this.groupBoxInput.Controls.Add(this.txtDiaChi);
+            this.groupBoxInput.Controls.Add(this.lblLop);
+            this.groupBoxInput.Controls.Add(this.txtLop);
+            this.groupBoxInput.Controls.Add(this.dtpNgaySinh);
+            this.groupBoxInput.Controls.Add(this.lblNgaySinh);
+            this.groupBoxInput.Controls.Add(this.txtHoTen);
+            this.groupBoxInput.Controls.Add(this.lblHoTen);
+            this.groupBoxInput.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBoxInput.Location = new System.Drawing.Point(15, 60);
+            this.groupBoxInput.Name = "groupBoxInput";
+            this.groupBoxInput.Size = new System.Drawing.Size(600, 110);
+            this.groupBoxInput.TabIndex = 1;
+            this.groupBoxInput.TabStop = false;
+            this.groupBoxInput.Text = "Thông tin sinh viên:";
+            // 
+            // lblDiaChi
+            // 
+            this.lblDiaChi.AutoSize = true;
+            this.lblDiaChi.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiaChi.Location = new System.Drawing.Point(310, 68);
+            this.lblDiaChi.Name = "lblDiaChi";
+            this.lblDiaChi.Size = new System.Drawing.Size(46, 15);
+            this.lblDiaChi.TabIndex = 7;
+            this.lblDiaChi.Text = "Địa chỉ:";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(380, 65);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(200, 23);
+            this.txtDiaChi.TabIndex = 6;
+            // 
+            // lblLop
+            // 
+            this.lblLop.AutoSize = true;
+            this.lblLop.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLop.Location = new System.Drawing.Point(310, 28);
+            this.lblLop.Name = "lblLop";
+            this.lblLop.Size = new System.Drawing.Size(31, 15);
+            this.lblLop.TabIndex = 5;
+            this.lblLop.Text = "Lớp:";
+            // 
+            // txtLop
+            // 
+            this.txtLop.Location = new System.Drawing.Point(380, 25);
+            this.txtLop.Name = "txtLop";
+            this.txtLop.Size = new System.Drawing.Size(200, 23);
+            this.txtLop.TabIndex = 4;
+            // 
+            // dtpNgaySinh
+            // 
+            this.dtpNgaySinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaySinh.Location = new System.Drawing.Point(85, 65);
+            this.dtpNgaySinh.Name = "dtpNgaySinh";
+            this.dtpNgaySinh.Size = new System.Drawing.Size(180, 23);
+            this.dtpNgaySinh.TabIndex = 3;
+            // 
+            // lblNgaySinh
+            // 
+            this.lblNgaySinh.AutoSize = true;
+            this.lblNgaySinh.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNgaySinh.Location = new System.Drawing.Point(10, 68);
+            this.lblNgaySinh.Name = "lblNgaySinh";
+            this.lblNgaySinh.Size = new System.Drawing.Size(63, 15);
+            this.lblNgaySinh.TabIndex = 2;
+            this.lblNgaySinh.Text = "Ngày sinh:";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(85, 25);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(180, 23);
+            this.txtHoTen.TabIndex = 1;
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoTen.Location = new System.Drawing.Point(10, 28);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(46, 15);
+            this.lblHoTen.TabIndex = 0;
+            this.lblHoTen.Text = "Họ tên:";
+            // 
+            // lblChucNang
+            // 
+            this.lblChucNang.AutoSize = true;
+            this.lblChucNang.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChucNang.Location = new System.Drawing.Point(15, 190);
+            this.lblChucNang.Name = "lblChucNang";
+            this.lblChucNang.Size = new System.Drawing.Size(73, 15);
+            this.lblChucNang.TabIndex = 2;
+            this.lblChucNang.Text = "Chức năng:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(110, 185);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 30);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(230, 185);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 30);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            //
+            this.btnDelete.Location = new System.Drawing.Point(350, 185);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 30);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(470, 185);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 30);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lvStudentList
+            // 
+            this.lvStudentList.HideSelection = false;
+            this.lvStudentList.Location = new System.Drawing.Point(15, 260);
+            this.lvStudentList.Name = "lvStudentList";
+            this.lvStudentList.Size = new System.Drawing.Size(600, 250);
+            this.lvStudentList.TabIndex = 7;
+            this.lvStudentList.UseCompatibleStateImageBehavior = false;
+            this.lvStudentList.SelectedIndexChanged += new System.EventHandler(this.lvStudentList_SelectedIndexChanged);
+            // 
+            // lblThongTinChung
+            // 
+            this.lblThongTinChung.AutoSize = true;
+            this.lblThongTinChung.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThongTinChung.Location = new System.Drawing.Point(15, 235);
+            this.lblThongTinChung.Name = "lblThongTinChung";
+            this.lblThongTinChung.Size = new System.Drawing.Size(142, 15);
+            this.lblThongTinChung.TabIndex = 8;
+            this.lblThongTinChung.Text = "Thông tin chung sinh viên:";
+            // 
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 521);
+            this.Controls.Add(this.lblThongTinChung);
+            this.Controls.Add(this.lvStudentList);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblChucNang);
+            this.Controls.Add(this.groupBoxInput);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Danh sách sinh viên";
+            this.groupBoxInput.ResumeLayout(false);
+            this.groupBoxInput.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.GroupBox groupBoxInput;
+        private System.Windows.Forms.Label lblHoTen;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Label lblNgaySinh;
+        private System.Windows.Forms.DateTimePicker dtpNgaySinh;
+        private System.Windows.Forms.Label lblLop;
+        private System.Windows.Forms.TextBox txtLop;
+        private System.Windows.Forms.Label lblDiaChi;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label lblChucNang;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListView lvStudentList;
+        private System.Windows.Forms.Label lblThongTinChung;
+    }
+}
---
+++ TranQuocKiet_1150080101_BTT6_TH2/TranQuocKiet_1150080101_BTT6_TH2/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+
+// Đảm bảo namespace trùng với tên dự án của bạn
+namespace TranQuocKiet_1150080101_BTT6_TH2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeListView();
+        }
+
+        private void InitializeListView()
+        {
+            // Thiết lập ListView ở chế độ Details và hiển thị lưới
+            lvStudentList.View = View.Details;
+            lvStudentList.GridLines = true;
+            lvStudentList.FullRowSelect = true; // Cho phép chọn toàn bộ dòng
+
+            // Thêm các cột vào ListView
+            // Đảm bảo kích thước phù hợp với hình minh họa
+            lvStudentList.Columns.Add("Họ tên", 150);
+            lvStudentList.Columns.Add("Ngày sinh", 100);
+            lvStudentList.Columns.Add("Lớp", 80);
+            lvStudentList.Columns.Add("Địa chỉ", 200);
+        }
+
+        // ----------------------------------------------------------------------
+        // Nút Thêm: Thêm thông tin sinh viên vào ListView
+        // ----------------------------------------------------------------------
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Yêu cầu: Kiểm tra thông tin họ tên sinh viên không được rỗng
+            if (string.IsNullOrWhiteSpace(txtHoTen.Text))
+            {
+                MessageBox.Show("Họ tên sinh viên không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtHoTen.Focus();
+                return;
+            }
+
+            // Tạo một đối tượng ListViewItem mới và thêm SubItems
+            ListViewItem item = new ListViewItem(txtHoTen.Text.Trim());
+            item.SubItems.Add(dtpNgaySinh.Value.ToShortDateString());
+            item.SubItems.Add(txtLop.Text.Trim());
+            item.SubItems.Add(txtDiaChi.Text.Trim());
+
+            lvStudentList.Items.Add(item);
+
+            // Xóa nội dung các ô input sau khi thêm
+            ClearInputFields();
+        }
+
+        // ----------------------------------------------------------------------
+        // Nút Xóa: Xóa dòng được chọn trên ListView
+        // ----------------------------------------------------------------------
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Yêu cầu: Kiểm tra người dùng đã chọn một dòng chưa
+            if (lvStudentList.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để xóa.", "Lỗi Xóa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var confirmResult = MessageBox.Show("Bạn có chắc chắn muốn xóa sinh viên này?", "Xác nhận Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirmResult == DialogResult.Yes)
+            {
+                // Xóa dòng được chọn
+                lvStudentList.SelectedItems[0].Remove();
+                ClearInputFields();
+            }
+        }
+
+        // ----------------------------------------------------------------------
+        // Nút Sửa: Cập nhật thông tin dòng được chọn
+        // ----------------------------------------------------------------------
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lvStudentList.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để sửa.", "Lỗi Sửa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtHoTen.Text))
+            {
+                MessageBox.Show("Họ tên sinh viên không được để trống!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtHoTen.Focus();
+                return;
+            }
+
+            // Lấy dòng được chọn
+            ListViewItem selectedItem = lvStudentList.SelectedItems[0];
+
+            // Cập nhật các SubItems
+            selectedItem.Text = txtHoTen.Text.Trim(); // Cột 0 (Ho ten)
+            selectedItem.SubItems[1].Text = dtpNgaySinh.Value.ToShortDateString(); // Cột 1 (Ngay sinh)
+            selectedItem.SubItems[2].Text = txtLop.Text.Trim(); // Cột 2 (Lop)
+            selectedItem.SubItems[3].Text = txtDiaChi.Text.Trim(); // Cột 3 (Dia chi)
+
+            MessageBox.Show("Thông tin sinh viên đã được cập nhật.", "Thành Công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // ----------------------------------------------------------------------
+        // Nút Thoát: Thoát khỏi chương trình
+        // ----------------------------------------------------------------------
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Thoát khỏi Form 2
+        }
+
+        // ----------------------------------------------------------------------
+        // Sự kiện SelectedIndexChanged: Hiển thị thông tin lên các ô input
+        // ----------------------------------------------------------------------
+        private void lvStudentList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Yêu cầu: Khi chọn 1 dòng trên listview thì nội dung của dòng được chọn sẽ được thể hiện trên các ô "Thông tin chung sinh viên".
+            if (lvStudentList.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = lvStudentList.SelectedItems[0];
+
+                txtHoTen.Text = selectedItem.SubItems[0].Text;
+
+                DateTime selectedDate;
+                if (DateTime.TryParse(selectedItem.SubItems[1].Text, out selectedDate))
+                {
+                    dtpNgaySinh.Value = selectedDate;
+                }
+                txtLop.Text = selectedItem.SubItems[2].Text;
+                txtDiaChi.Text = selectedItem.SubItems[3].Text;
+            }
+            else
+            {
+                ClearInputFields();
+            }
+        }
+
+        // Hàm hỗ trợ xóa các ô input
+        private void ClearInputFields()
+        {
+            txtHoTen.Clear();
+            dtpNgaySinh.Value = DateTime.Now;
+            txtLop.Clear();
+            txtDiaChi.Clear();
+            // Không nên focus nếu ClearInputFields được gọi sau khi xóa/sửa.
+            // Để lại focus cho người dùng tùy ý.
+        }
+    }
+}
